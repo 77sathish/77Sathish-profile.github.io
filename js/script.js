bc997dca51d6965c55ec1e7869e299bd6c0c8504
77sathish <116945191+77sathish@users.noreply.github.com>
2026-08-14 19:51:58 +0530

    Add sidebar toggle functionality with menu button

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector(".menu-button");
+
+const sidebar = document.querySelector(".sidebar");
+
+menuButton.addEventListener("click", () => {
+
+    sidebar.classList.toggle("active");
+
+});
